48992b208076cd317240ae86a4ec95ee1e6d2947
Jace <638764+jacebenson@users.noreply.github.com>
2023-09-17 00:43:02 -0500

    got links working quickly

--- scripts/getProperties.js
+++ scripts/getProperties.js
@@ -112,12 +112,8 @@ async function makeSimpleProperties() {
     returnProperties.sort((a, b) => {
         let nameA = a.name.toUpperCase(); // ignore upper and lowercase
         let nameB = b.name.toUpperCase(); // ignore upper and lowercase
-        if (nameA < nameB) {
-            return -1;
-        }
-        if (nameA > nameB) {
-        return 1;
-        }
+        if (nameA < nameB) { return -1; }
+        if (nameA > nameB) { return 1; }
         // names must be equal
         return 0;
     });
