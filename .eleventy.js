7e716038c40025f472878fd2eea3814890f5d296
Jace <638764+jacebenson@users.noreply.github.com>
2023-11-08 12:48:40 -0600

    added sitemap

--- .eleventy.js
+++ .eleventy.js
@@ -19,7 +19,13 @@ module.exports = function (eleventyConfig) {
     permalink: anchor.permalink.headerLink()
   });
   eleventyConfig.setLibrary("md", markdownLibrary);
-
+  eleventyConfig.addFilter("lastmodDate", function(value){
+    // return should be yyyy-mm-dd with padded 0's for mm and dd
+    let date = new Date(value);
+    let mm = date.getMonth() + 1;
+    let dd = date.getDate();
+    return `${date.getFullYear()}-${mm.toString().padStart(2, "0")}-${dd.toString().padStart(2, "0")}`;
+  })
   return {
     dir: {
       input: "src",
